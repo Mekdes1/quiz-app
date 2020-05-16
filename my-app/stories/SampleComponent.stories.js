c899a08135d558d8179ab346232d9ff951a61355
Mekdes <yesmek@yahoo.com>
2020-05-16 18:08:47 +0200

    SampleComponents.stories

--- my-app/stories/SampleComponent.stories.js
+++ my-app/stories/SampleComponent.stories.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import SampleComponent from './SampleComponent';
+import React from "react";
+import SampleComponent from "./SampleComponent";
 
-export default { title: 'Sample Component' };
+export default { title: "Sample Component" };
 
 export const Component = () => <SampleComponent title="Working" />;
