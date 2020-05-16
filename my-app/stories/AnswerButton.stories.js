106f4692c94e62e3b911befd68279d80afd0c24f
Mekdes <yesmek@yahoo.com>
2020-05-16 18:14:13 +0200

    AnswerButton.stories.js

---
+++ my-app/stories/AnswerButton.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import AnswerButton from "./AnswerButtonComponent";
+
+export default { title: "Answer button story" };
+
+export const AnswerButtonStory = () => <AnswerButton />;
