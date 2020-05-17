44281e1445ecd560209fe954fe9362748123df16
Mekdes <yesmek@yahoo.com>
2020-05-17 20:48:03 +0200

    removed my-app folder

--- my-app/stories/AnswerButton.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import AnswerButton from "./AnswerButtonComponent";
-
-export default { title: "Answer button story" };
-
-export const AnswerButtonStory = () => <AnswerButton />;
--- my-app/stories/SampleQuiz.stories.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import SampleQuizComponent from "./SampleQuizComponent";
-export default { title: "Sample Quiz Component" };
-
-export const QuizComponent = () => <SampleQuizComponent />;
