096c9808d46510cae3db756348ae3a18fe89ea6a
Mekdes <yesmek@yahoo.com>
2020-05-26 12:09:55 +0200

    added option button stories

---
+++ stories/OptionButtonsComponent.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import DisplayOptions from "./OptionButtons";
+export default { title: "Display options button" };
+
+export const DisplayOptionsComponent = () => <DisplayOptions />;
