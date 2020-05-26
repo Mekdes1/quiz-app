c0bb3ebae86d5cd480927fa7a2c646130eb2e883
Mekdes <yesmek@yahoo.com>
2020-05-26 12:05:00 +0200

    added new story component

--- stories/QuzzAppStoryComponents.stories.js
+++ stories/QuzzAppStoryComponents.stories.js
@@ -2,14 +2,10 @@ import React from "react";
 import Title from "./Title";
 
 import Question from "./QuestionStoryComponent";
-import FirstAnswer from "./FirstAnswerComponent";
-import SecondAnswer from "./SecondAnswerComponent";
-import ThirdAnswer from "./ThirdAnswerComponent";
+import DisplayOptions from "./OptionButtons";
 
 export default { title: "Quzz app story components" };
 
 export const TitleComponent = () => <Title />;
 export const QuestionComponent = () => <Question />;
-export const FirstAnswerComponent = () => <FirstAnswer />;
-export const SecondAnswerComponent = () => <SecondAnswer />;
-export const ThirdComponent = () => <ThirdAnswer />;
+export const DisplayOptionsComponent = () => <DisplayOptions />;
