491049ad18fcfd5a75a46932944c37a1646cd069
Mekdes <yesmek@yahoo.com>
2020-05-26 12:08:35 +0200

    quizService answer update

--- stories/quizService/index.js
+++ stories/quizService/index.js
@@ -1,37 +1,37 @@
 const qBank = [
   {
     question:
-      'Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?   ',
+      "Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?   ",
     answers: [
-      { answer: 'Richard Branson', correctAnswer: 'loding' },
-      { answer: 'Alan Sugar', correctAnswer: 'loding' },
-      { answer: 'Donald Trump', correctAnswer: 'loding' },
+      { answer: "Richard Branson", correctAnswer: "loding" },
+      { answer: "Alan Sugar", correctAnswer: "loding" },
+      { answer: "Donald Trump", correctAnswer: "loding" },
     ],
-    correct: 'Richard Branson',
-    questionId: '099099',
+    correct: "Richard Branson",
+    questionId: "099099",
   },
   {
     question:
       'Where is the train station "Llanfair­pwllgwyngyll­gogery­chwyrn­drobwll­llan­tysilio­gogo­goch"?',
     answers: [
-      { answer: 'Wales', correctAnswer: 'loding' },
-      { answer: 'Czech Republic', correctAnswer: 'loding' },
-      { answer: 'Denmark', correctAnswer: 'loding' },
+      { answer: "Wales", correctAnswer: "loding" },
+      { answer: "Czech Republic", correctAnswer: "loding" },
+      { answer: "Denmark", correctAnswer: "loding" },
     ],
 
-    correct: 'Wales',
-    questionId: '183452',
+    correct: "Wales",
+    questionId: "183452",
   },
   {
     question:
-      'Which company did Valve cooperate with in the creation of the Vive?',
+      "Which company did Valve cooperate with in the creation of the Vive?",
     answers: [
-      { answer: 'HTC', correctAnswer: 'loding' },
-      { answer: 'Oculus', correctAnswer: 'loding' },
-      { answer: 'Google', correctAnswer: 'loding' },
+      { answer: "HTC", correctAnswer: "loding" },
+      { answer: "Oculus", correctAnswer: "loding" },
+      { answer: "Google", correctAnswer: "loding" },
     ],
-    correct: 'HTC',
-    questionId: '267908',
+    correct: "HTC",
+    questionId: "267908",
   },
 ];
 
