4d525210c53152abc3549a5b68b857c639164be2
Mekdes <yesmek@yahoo.com>
2020-05-26 12:04:21 +0200

    removed first answer component

--- stories/FirstAnswerComponent.stories.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import FirstAnswer from "./FirstAnswerComponent";
-export default { title: "First answer component" };
-
-export const FirstAnswerComponent = () => <FirstAnswer />;
