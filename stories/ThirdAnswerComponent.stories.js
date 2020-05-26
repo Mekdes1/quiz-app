f3317b0bade3a1a8c65cd401901d6ba17f6eaf52
Mekdes <yesmek@yahoo.com>
2020-05-26 12:07:02 +0200

    removed third answer story component

--- stories/ThirdAnswerComponent.stories.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ThirdAnswer from "./ThirdAnswerComponent";
-export default { title: "Third answer component" };
-
-export const ThirdAnswerComponent = () => <ThirdAnswer />;
