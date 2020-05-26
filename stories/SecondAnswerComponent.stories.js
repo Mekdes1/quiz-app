a6ba1501be2699d28c5470c7da6baeb4c2e32e8d
Mekdes <yesmek@yahoo.com>
2020-05-26 12:06:13 +0200

    removed second answer stories component

--- stories/SecondAnswerComponent.stories.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import SecondAnswer from "./SecondAnswerComponent";
-export default { title: "Second answer component" };
-
-export const SecondAnswerComponent = () => <SecondAnswer />;
